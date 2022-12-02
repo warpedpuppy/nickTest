9a1a1af8a3ebde2aeeb4d30d9b80c3942beec86e
ted <ted@warpedpuppy.com>
2022-12-02 16:00:11 -0500

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
+app.use(cors());
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
