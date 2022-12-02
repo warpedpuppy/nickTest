c9d503ae24b515f332a28210642daffdcfaabee3
ted <ted@warpedpuppy.com>
2022-12-02 13:55:23 -0500

    added public

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-	res.status(200).send('I exist')
-})
+
+// app.get('/', (req, res) => {
+// 	res.status(200).send('I exist')
+// })
 
 app.listen(8080, () => console.log('listening'))
