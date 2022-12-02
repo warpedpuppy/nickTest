eed1a370a1d70d50c7a712b585055dee6685e60c
ted <ted@warpedpuppy.com>
2022-12-02 13:57:28 -0500

    add login

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path')
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
-// app.get('/', (req, res) => {
-// 	res.status(200).send('I exist')
-// })
+app.post('/login', (req, res) => {
+	res.status(200).send('I exist')
+})
 
 app.listen(8080, () => console.log('listening'))
