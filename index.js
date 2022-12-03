6eafcd8c3900f2719c71ef04452e771f611e0676
ted <ted@warpedpuppy.com>
2022-12-02 19:19:33 -0500

    removed public

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const path = require('path');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
-app.use('/public', express.static(path.join(__dirname, 'public')))
+// app.use('/public', express.static(path.join(__dirname, 'public')))
 
-
+app.get('/', (req, res) => {
+	console.log('req = ', req)
+	res.status(200).send('I exist -- get')
+})
 app.post('/login', (req, res) => {
 	console.log('req = ', req)
-	res.status(200).send('I exist')
+	res.status(200).send('I exist -- login post')
 })
 
 app.listen(8080, () => console.log('listening'))
