3ec324994a6469e14345aafdb6d25c2da29a0439
ted <ted@warpedpuppy.com>
2022-12-02 19:09:59 -0500

    added json handler

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 app.use(cors());
+app.use(express.json())
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
 app.post('/login', (req, res) => {
+	console.log('req = ', req)
 	res.status(200).send('I exist')
 })
 
